e7981dbf553d07bcb64b8cc172fb1ef8548cafc3
brenoluca21 <reludia11@gmail.com>
2025-08-19 21:17:10 -0300

    EndPoint para Buscar as Listas de GameList

--- src/main/java/com/devsuperior/dslist/dto/GameDTO.java
+++ src/main/java/com/devsuperior/dslist/dto/GameDTO.java
@@ -1,68 +1,32 @@
-package com.devsuperior.dslist.entities;
+package com.devsuperior.dslist.dto;
 
-import jakarta.persistence.*;
 
-import java.util.Objects;
+import com.devsuperior.dslist.entities.Game;
+import org.springframework.beans.BeanUtils;
 
-@Entity
-@Table(name = "tb_game")
-public class Game {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+public class GameDTO {
     private Long id;
     private String title;
-
-    @Column(name = "game_year")
     private Integer year;
     private String genre;
     private String platforms;
     private Double score;
     private String imgUrl;
-
-    @Column(columnDefinition = "TEXT")
     private String shortDescription;
-
-    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
-    public Game(){}
+    public GameDTO(){}
 
-    public Game(long id, String title, Integer year, String genre, String platforms, Double score,
-                String imgUrl, String shortDescription, String longDescription) {
-        this.id = id;
-        this.title = title;
-        this.year = year;
-        this.genre = genre;
-        this.platforms = platforms;
-        this.score = score;
-        this.imgUrl = imgUrl;
-        this.shortDescription = shortDescription;
-        this.longDescription = longDescription;
+    public GameDTO(Game entity){
+        //apenas para quem tem os mesmo nomes de atributos, com isso precisa de getter e setter
+        BeanUtils.copyProperties(entity, this);
     }
 
-    @Override
-    public boolean equals(Object object) {
-        if(this == object)
-            return true;
-        if(object == null)
-            return false;
-        if(getClass() != object.getClass())
-            return false;
-        Game other = (Game) object;
-        return Objects.equals(id, other.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(id);
-    }
-
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
--- src/main/java/com/devsuperior/dslist/services/GameListService.java
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -1,33 +1,28 @@
 package com.devsuperior.dslist.services;
 
 import com.devsuperior.dslist.dto.GameDTO;
+import com.devsuperior.dslist.dto.GameListDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.entities.GameList;
+import com.devsuperior.dslist.repositories.GameListRepository;
 import com.devsuperior.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
 @Service
-public class GameService {
+public class GameListService {
 
     @Autowired
-    private GameRepository gameRepository;
+    private GameListRepository gameListRepository;
 
     @Transactional(readOnly = true)
-    public List<GameMinDTO> findAll(){
-        List<Game> result = gameRepository.findAll();
-        List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
-        return dto;
-    }
-
-    @Transactional(readOnly = true)
-    public GameDTO findById(Long id){
-        Game result = gameRepository.findById(id).get();
-        GameDTO dto = new GameDTO(result);
+    public List<GameListDTO> findAll(){
+        List<GameList> result = gameListRepository.findAll();
+        List<GameListDTO> dto = result.stream().map(x -> new GameListDTO(x)).toList();
         return dto;
     }
 
